6a2d17001bb1a7ef78e5202d5b76fc2a82149a2d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-23 11:03:03 -0700

    Implementing role parsing in ArtistCredit.

--- discograph/models/ArtistCredit.py
+++ discograph/models/ArtistCredit.py
@@ -10,7 +10,7 @@ class ArtistCredit(Model, mongoengine.EmbeddedDocument):
     anv = mongoengine.StringField()
     artist = mongoengine.ReferenceField('Artist')
     join = mongoengine.StringField()
-    role = mongoengine.StringField()
+    roles = mongoengine.ListField(mongoengine.StringField())
     tracks = mongoengine.StringField()
 
     ### PUBLIC METHODS ###
@@ -27,12 +27,40 @@ class ArtistCredit(Model, mongoengine.EmbeddedDocument):
         document = cls(**data)
         return document
 
+    @classmethod
+    def element_to_roles(cls, element):
+        if element is not None and element.text:
+            return cls.parse_roles(element.text)
+        return []
+
+    @classmethod
+    def parse_roles(cls, text):
+        roles = []
+        role = ''
+        in_bracket = False
+        for c in text:
+            if not in_bracket and c == '[':
+                in_bracket = True
+            elif in_bracket and c == ']':
+                in_bracket = False
+            elif not in_bracket and c == ',':
+                role = role.strip()
+                if role:
+                    roles.append(role)
+                role = ''
+                continue
+            role += c
+        role = role.strip()
+        if role:
+            roles.append(role)
+        return roles
+
 
 ArtistCredit._tags_to_fields_mapping = {
     'anv': ('anv', Bootstrap.element_to_string),
     'id': ('discogs_id', Bootstrap.element_to_integer),
     'join': ('join', Bootstrap.element_to_string),
     'name': ('name', Bootstrap.element_to_string),
-    'role': ('role', Bootstrap.element_to_string),
+    'role': ('roles', ArtistCredit.element_to_roles),
     'tracks': ('tracks', Bootstrap.element_to_string),
     }
