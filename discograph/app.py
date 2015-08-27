e398f16eecc6ef7cfcb1e726bebfe825d72ed641
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-26 23:09:47 -0700

    Added random cluster route to app.

--- discograph/app.py
+++ discograph/app.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import traceback
 from flask import (
     Flask,
     abort,
@@ -18,7 +19,34 @@ def route__api__cluster__artist_id(artist_id):
         artist = discograph.models.Artist.objects.get(discogs_id=artist_id)
     except:
         abort(404)
-    artist_graph = discograph.graphs.ArtistMembershipGrapher([artist], 3)
+    artist_graph = discograph.graphs.ArtistMembershipGrapher(
+        [artist], 8)
+    data = artist_graph.to_json(max_nodes=100)
+    return jsonify(data)
+
+
+@app.route('/api/cluster/random', methods=['GET'])
+def route__api__cluster__random():
+    import discograph
+    import random
+    discograph.connect()
+    count = discograph.models.Artist.objects.count()
+    artist = None
+    while artist is None:
+        discogs_id = random.randrange(1, count)
+        print(discogs_id)
+        try:
+            artist = discograph.models.Artist.objects.get(discogs_id=discogs_id)
+        except:
+            traceback.print_exc()
+            artist = None
+        if artist is None:
+            continue
+        print(artist.name)
+        if not artist.members and not artist.groups:
+            artist = None
+    artist_graph = discograph.graphs.ArtistMembershipGrapher(
+        [artist], 8)
     data = artist_graph.to_json(max_nodes=100)
     return jsonify(data)
 
