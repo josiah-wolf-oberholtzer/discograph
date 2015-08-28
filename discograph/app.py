652213a0bb9ee4b2db961c18aa900dd3118a9c2b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-28 15:07:35 -0700

    Flask application clears cache on startup.

--- discograph/app.py
+++ discograph/app.py
@@ -12,34 +12,7 @@ from werkzeug.contrib.cache import MemcachedCache
 
 app = Flask(__name__)
 cache = MemcachedCache(['127.0.0.1:11211'])
-
-
-@app.route('/api/cluster/<int:artist_id>', methods=['GET'])
-@app.route('/api/cluster/<int:artist_id>/', methods=['GET'])
-def route__api__cluster__artist_id(artist_id):
-    import discograph
-    key = '/api/cluster/{}'.format(artist_id)
-    data = cache.get(key)
-    if data is not None:
-        print('CACHE HIT:', key)
-        return jsonify(data)
-    print('CACHE MISS:', key)
-    discograph.connect()
-    try:
-        artist = discograph.models.Artist.objects.get(discogs_id=artist_id)
-    except:
-        abort(404)
-    artist_graph = discograph.graphs.ArtistMembershipGrapher(
-        [artist], 12)
-    data = artist_graph.to_json(max_nodes=100)
-    cache.set(key, data, timeout=60 * 60)
-    return jsonify(data)
-
-
-@app.route('/<int:artist_id>', methods=['GET'])
-@app.route('/<int:artist_id>/', methods=['GET'])
-def route__artist_id(artist_id):
-    return render_template('index.html', artist_id=artist_id)
+cache.clear()
 
 
 @app.route('/')
@@ -65,6 +38,34 @@ def route():
             artist = None
     artist_id = artist.discogs_id
     return redirect('/{}'.format(artist_id), code=302)
+
+
+@app.route('/<int:artist_id>', methods=['GET'])
+@app.route('/<int:artist_id>/', methods=['GET'])
+def route__artist_id(artist_id):
+    return render_template('index.html', artist_id=artist_id)
+
+
+@app.route('/api/cluster/<int:artist_id>', methods=['GET'])
+@app.route('/api/cluster/<int:artist_id>/', methods=['GET'])
+def route__api__cluster__artist_id(artist_id):
+    import discograph
+    key = '/api/cluster/{}'.format(artist_id)
+    data = cache.get(key)
+    if data is not None:
+        print('CACHE HIT:', key)
+        return jsonify(data)
+    print('CACHE MISS:', key)
+    discograph.connect()
+    try:
+        artist = discograph.models.Artist.objects.get(discogs_id=artist_id)
+    except:
+        abort(404)
+    artist_graph = discograph.graphs.ArtistMembershipGrapher(
+        [artist], 12)
+    data = artist_graph.to_json(max_nodes=100)
+    cache.set(key, data, timeout=60 * 60)
+    return jsonify(data)
 
 
 if __name__ == '__main__':
