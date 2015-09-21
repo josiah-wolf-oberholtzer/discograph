fb5fa12e7addc72ed3f1c7b96a3ba59e1874260b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-09-20 17:20:55 -0700

    De-crufted Flask app.

--- discograph/app.py
+++ discograph/app.py
@@ -3,7 +3,6 @@ import Levenshtein
 import abjad
 import discograph
 import mongoengine
-import random
 import re
 from flask import (
     Flask,
@@ -12,12 +11,13 @@ from flask import (
     redirect,
     render_template,
     )
-from werkzeug.contrib.cache import MemcachedCache
+#from werkzeug.contrib.cache import MemcachedCache
 
 
 app = Flask(__name__)
-cache = MemcachedCache(['127.0.0.1:11211'])
-cache.clear()
+#cache = MemcachedCache(['127.0.0.1:11211'])
+#cache.clear()
+#cache = None
 mongoengine.connect('discograph')
 
 
@@ -32,13 +32,8 @@ def route__index():
 
 @app.route('/random')
 def route__random():
-    count = discograph.models.Artist.objects.count()
-    index = random.randrange(count)
-    artist = discograph.models.Artist.objects[index]
-    while not artist.members and not artist.groups:
-        index = random.randrange(count)
-        artist = discograph.models.Artist.objects[index]
-    artist_id = artist.discogs_id
+    artist = discograph.models.SQLArtist.get_random()
+    artist_id = artist.id
     return redirect('/artist/{}'.format(artist_id), code=302)
 
 
@@ -57,33 +52,24 @@ def route__artist_id(artist_id):
 
 @app.route('/api/artist/network/<int:artist_id>', methods=['GET'])
 def route__api__cluster(artist_id):
-    cache_key = 'cache:/api/artist/network/{}'.format(artist_id)
-    data = cache.get(cache_key)
-    if data is not None:
-        print('NETWORK CACHE HIT:', cache_key)
-        return jsonify(data)
-    print('NETWORK CACHE MISS:', cache_key)
     try:
         artist = discograph.models.Artist.objects.get(discogs_id=artist_id)
     except:
         abort(404)
+    role_names = [
+        'Alias',
+        'Member Of',
+        'Producer',
+        'Remix',
+        ]
     relation_grapher = discograph.RelationGrapher(
         entities=[artist],
-        cache=cache,
         degree=12,
         max_nodes=100,
-        role_names=[
-            'Alias',
-            'Member Of',
-            'Released On',
-            'Sublabel Of',
-            'Producer',
-            'Remix',
-            ],
+        role_names=role_names,
         )
     with abjad.systemtools.Timer(exit_message='Network query time:'):
         data = relation_grapher.get_network()
-    cache.set(cache_key, data)
     return jsonify(data)
 
 
@@ -92,18 +78,7 @@ urlify_pattern = re.compile(r"\s+", re.MULTILINE)
 
 @app.route('/api/search/<search_string>', methods=['GET'])
 def route__api__search(search_string):
-    cache_key = 'cache:/api/search/{}'.format(search_string)
-    cache_key = urlify_pattern.sub('+', cache_key)
-    print(cache_key)
-    data = cache.get(cache_key)
-    if data is not None:
-        #print('CACHE HIT:', cache_key)
-        return jsonify(data)
-    #print('CACHE MISS:', cache_key)
     data = discograph.models.Artist.search_text(search_string, limit=50)
-    #query = discograph.models.Artist.objects(name__istartswith=search_string)
-    #query = query.only('discogs_id', 'name').limit(50)
-    #data = list(query.as_pymongo())
     data.sort(key=lambda x: Levenshtein.distance(x['name'], search_string))
     print(search_string)
     for datum in data:
@@ -111,7 +86,6 @@ def route__api__search(search_string):
         del(datum['_id'])
         print(datum)
     data = {'results': data}
-    cache.set(cache_key, data)
     return jsonify(data)
 
 
