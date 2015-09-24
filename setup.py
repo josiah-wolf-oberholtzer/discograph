97f4a6b1603c38ac732501645b8909db64f22965
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-09-24 13:35:24 -0700

    Updated dependencies.

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
         install_requires=[
             'abjad',
             'flask',
+            'Flask-Mobility',
             'gunicorn',
             'mongoengine',
             'peewee',
