54d53867e523fff64f683b362492f9ea54d27e40
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-09-30 10:22:55 -0700

    DG.js looks for dgData on page load.

--- discograph/static/js/dg/dg.js
+++ discograph/static/js/dg/dg.js
@@ -2,10 +2,18 @@ var dg = (function(dg){
     dg.init = function() {
         dg.graph.init();
         dg.typeahead.init();
+
+        /*
         var entityKey = d3.select("body").attr("id");
         if (entityKey) {
             dg.navigateGraph(d3.select("body").attr("id"));
         }
+        */
+        if (dgData) {
+            dg.history.pushState(dgData.center);
+            dg.handleNewGraphData(null, dgData);
+        }
+
         console.log('discograph initialized.')
     }
     return dg;
