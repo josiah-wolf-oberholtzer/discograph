e03b02a09d52456023fc74a0f4b0dd8282b763bd
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-10-01 10:46:22 -0700

    Cleaned-up library init. Created DB-specific subpackages.

--- discograph/library/sqlite/__init__.py
+++ discograph/library/sqlite/__init__.py
@@ -5,7 +5,3 @@ systemtools.ImportManager.import_structured_package(
     __path__[0],
     globals(),
     )
-
-from discograph.library.mongo import *
-from discograph.library.postgres import *
-from discograph.library.sqlite import *
