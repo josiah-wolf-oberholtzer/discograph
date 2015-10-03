0ee9bca8ba363b993361882476bcd82f1152f6ea
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-10-02 22:34:50 -0700

    Removed three Sqlite models.

--- discograph/library/sqlite/SqliteFTSArtist.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from discograph.library.sqlite.SqliteArtist import SqliteArtist
-from playhouse import sqlite_ext
-
-
-class SqliteFTSArtist(SqliteArtist, sqlite_ext.FTSModel):
-
-    class Meta:
-        db_table = 'sqlftsartist'
