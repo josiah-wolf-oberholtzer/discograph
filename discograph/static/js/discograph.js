7b96abe15be97c246924b55ec017ad8a255684f1
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-10-03 17:53:31 -0700

    Refactored JS.

--- discograph/static/js/discograph.js
+++ discograph/static/js/discograph.js
@@ -26,88 +26,6 @@ dg.network = {
     linkLayer: null,
 };
 
-function dg_svg_setupDefs() {
-    var defs = d3.select("#svg").append("defs");
-    // ARROWHEAD
-    defs.append("marker")
-        .attr("id", "arrowhead")
-        .attr("viewBox", "-5 -5 10 10")
-        .attr("refX", 4)
-        .attr("refY", 0)
-        .attr("markerWidth", 8)
-        .attr("markerHeight", 8)
-        .attr("markerUnits", "strokeWidth")
-        .attr("orient", "auto")
-        .append("path")
-        .attr("d", "M 0,0 m -5,-5 L 5,0 L -5,5 L -2.5,0 L -5,-5 Z")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-linejoin", "round");
-    // AGGREGATE
-    defs.append("marker")
-        .attr("id", "aggregate")
-        .attr("viewBox", "-5 -5 10 10")
-        .attr("refX", 5)
-        .attr("refY", 0)
-        .attr("markerWidth", 8)
-        .attr("markerHeight", 8)
-        .attr("markerUnits", "strokeWidth")
-        .attr("orient", "auto")
-        .append("path")
-        .attr("d", "M 0,0 m 5,0 L 0,-3 L -5,0 L 0,3 L 5,0 Z")
-        .attr("fill", "#fff")
-        .attr("stroke", "#000")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-width", 1.5);
-    // GAUSSIAN BLUR
-    var filter = defs.append("filter")
-        .attr("id", "drop-shadow")
-        .attr("y", "-50%")
-        .attr("x", "-50%")
-        .attr("height", "300%")
-        .attr("width", "300%");
-    filter.append("feGaussianBlur")
-        .attr("in", "SourceAlpha")
-        .attr("stdDeviation", 3)
-        .attr("result", "blur");
-    filter.append("feOffset")
-        .attr("in", "blur")
-        .attr("dx", 4)
-        .attr("dy", 4)
-        .attr("result", "offsetBlur");
-    var feComponentTransfer = filter.append("feComponentTransfer")
-        .attr("in", "offsetBlur")
-        .attr("result", "lightenedBlur");
-    feComponentTransfer.append("feFuncA")
-        .attr("type", "linear")
-        .attr("slope", 0.25)
-        .attr("intercept", 0);
-    var feMerge = filter.append("feMerge");
-    feMerge.append("feMergeNode")
-        .attr("in", "lightenedBlur")
-    feMerge.append("feMergeNode")
-        .attr("in", "SourceGraphic");
-    // RADIAL GRADIENT
-    var gradient = defs.append('radialGradient')
-        .attr('id', 'radial-gradient');
-    gradient.append('stop')
-        .attr('offset', '0%')
-        .attr('stop-color', '#333')
-        .attr('stop-opacity', '100%');
-    gradient.append('stop')
-        .attr('offset', '50%')
-        .attr('stop-color', '#333')
-        .attr('stop-opacity', '33%');
-    gradient.append('stop')
-        .attr('offset', '75%')
-        .attr('stop-color', '#333')
-        .attr('stop-opacity', '11%');
-    gradient.append('stop')
-        .attr('offset', '100%')
-        .attr('stop-color', '#333')
-        .attr('stop-opacity', '0%');
-}
-
 function dg_network_setupForceLayout() {
     return d3.layout.force()
         .nodes(dg.network.nodes)
@@ -880,11 +798,10 @@ function dg_network_init() {
     dg.network.forceLayout = dg_network_setupForceLayout();
 }
 
-function dg_init() {
+function dg_svg_init() {
     d3.selection.prototype.moveToFront = function() {
         return this.each(function(){ this.parentNode.appendChild(this); });
     };
-    window.addEventListener("popstate", dg_history_onPopState);
     var w = window,
         d = document,
         e = d.documentElement,
@@ -911,6 +828,92 @@ function dg_init() {
         .attr("width", dg.network.dimensions[0])
         .attr("height", dg.network.dimensions[1]);
     dg_svg_setupDefs();
+}
+
+function dg_svg_setupDefs() {
+    var defs = d3.select("#svg").append("defs");
+    // ARROWHEAD
+    defs.append("marker")
+        .attr("id", "arrowhead")
+        .attr("viewBox", "-5 -5 10 10")
+        .attr("refX", 4)
+        .attr("refY", 0)
+        .attr("markerWidth", 8)
+        .attr("markerHeight", 8)
+        .attr("markerUnits", "strokeWidth")
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M 0,0 m -5,-5 L 5,0 L -5,5 L -2.5,0 L -5,-5 Z")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-linejoin", "round");
+    // AGGREGATE
+    defs.append("marker")
+        .attr("id", "aggregate")
+        .attr("viewBox", "-5 -5 10 10")
+        .attr("refX", 5)
+        .attr("refY", 0)
+        .attr("markerWidth", 8)
+        .attr("markerHeight", 8)
+        .attr("markerUnits", "strokeWidth")
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M 0,0 m 5,0 L 0,-3 L -5,0 L 0,3 L 5,0 Z")
+        .attr("fill", "#fff")
+        .attr("stroke", "#000")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 1.5);
+    // GAUSSIAN BLUR
+    var filter = defs.append("filter")
+        .attr("id", "drop-shadow")
+        .attr("y", "-50%")
+        .attr("x", "-50%")
+        .attr("height", "300%")
+        .attr("width", "300%");
+    filter.append("feGaussianBlur")
+        .attr("in", "SourceAlpha")
+        .attr("stdDeviation", 3)
+        .attr("result", "blur");
+    filter.append("feOffset")
+        .attr("in", "blur")
+        .attr("dx", 4)
+        .attr("dy", 4)
+        .attr("result", "offsetBlur");
+    var feComponentTransfer = filter.append("feComponentTransfer")
+        .attr("in", "offsetBlur")
+        .attr("result", "lightenedBlur");
+    feComponentTransfer.append("feFuncA")
+        .attr("type", "linear")
+        .attr("slope", 0.25)
+        .attr("intercept", 0);
+    var feMerge = filter.append("feMerge");
+    feMerge.append("feMergeNode")
+        .attr("in", "lightenedBlur")
+    feMerge.append("feMergeNode")
+        .attr("in", "SourceGraphic");
+    // RADIAL GRADIENT
+    var gradient = defs.append('radialGradient')
+        .attr('id', 'radial-gradient');
+    gradient.append('stop')
+        .attr('offset', '0%')
+        .attr('stop-color', '#333')
+        .attr('stop-opacity', '100%');
+    gradient.append('stop')
+        .attr('offset', '50%')
+        .attr('stop-color', '#333')
+        .attr('stop-opacity', '33%');
+    gradient.append('stop')
+        .attr('offset', '75%')
+        .attr('stop-color', '#333')
+        .attr('stop-opacity', '11%');
+    gradient.append('stop')
+        .attr('offset', '100%')
+        .attr('stop-color', '#333')
+        .attr('stop-opacity', '0%');
+}
+
+function dg_init() {
+    dg_svg_init();
     dg_network_init();
     dg_typeahead_init();
     if (dgData) {
@@ -934,6 +937,7 @@ function dg_init() {
             event.preventDefault();
         });
     }());
+    window.addEventListener("popstate", dg_history_onPopState);
     console.log('discograph initialized.');
 }
 
