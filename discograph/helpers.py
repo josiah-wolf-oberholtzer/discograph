55014612ba20a796b4547a9b0299ec1b64d0f888
Josiah Oberholtzer <joberholtzer@discogsinc.com>
2015-11-17 19:38:24 -0800

    Improved random entity helper.

--- discograph/helpers.py
+++ discograph/helpers.py
@@ -35,18 +35,18 @@ def get_entity(entity_type, entity_id):
 def get_network(entity_id, entity_type, on_mobile=False, cache=True, roles=None):
     import discograph
     assert entity_type in ('artist', 'label')
+    template = 'discograph:/api/{entity_type}/network/{entity_id}'
+    cache_key = discograph.RelationGrapher.make_cache_key(
+        template,
+        entity_type,
+        entity_id,
+        roles=roles,
+        )
+    if on_mobile:
+        template = '{}/mobile'.format(template)
+    cache_key = cache_key.format(entity_type, entity_id)
     cache = False
     if cache:
-        template = 'discograph:/api/{entity_type}/network/{entity_id}'
-        if on_mobile:
-            template = '{}/mobile'.format(template)
-        cache_key = discograph.RelationGrapher.make_cache_key(
-            template,
-            entity_type,
-            entity_id,
-            roles=roles,
-            )
-        cache_key = cache_key.format(entity_type, entity_id)
         data = discograph.RelationGrapher.cache_get(cache_key)
         if data is not None:
             return data
@@ -75,7 +75,12 @@ def get_network(entity_id, entity_type, on_mobile=False, cache=True, roles=None)
 
 def get_random_entity(roles=None):
     import discograph
-    if roles:
+    structural_roles = [
+        'Alias',
+        'Member Of',
+        'Sublabel Of',
+        ]
+    if roles and any(_ not in structural_roles for _ in roles):
         relation = discograph.PostgresRelation.get_random(roles=roles)
         entity_choice = random.randint(1, 2)
         if entity_choice == 1:
@@ -155,10 +160,10 @@ def parse_request_args(args):
 
 def search_entities(search_string, cache=True):
     import discograph
+    cache_key = 'discograph:/api/search/{}'.format(
+        urlify_pattern.sub('+', search_string))
     cache = False
     if cache:
-        cache_key = 'discograph:/api/search/{}'.format(
-            urlify_pattern.sub('+', search_string))
         data = discograph.RelationGrapher.cache_get(cache_key)
         if data is not None:
             print('{}: CACHED'.format(cache_key))
